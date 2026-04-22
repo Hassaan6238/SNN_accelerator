15063cb79df11ed6be2300e36a9b64630e8378ff
hassan6238 <hassaanahmed6238@gmail.com>
2026-04-22 21:58:58 +0500

    package file added

---
+++ rtl/servant/config_pkg.sv
@@ -0,0 +1,23 @@
+package config_pkg;
+
+        parameter int DW = 8;
+        parameter int INPUT_CHANNELS = 16;
+        parameter int INPUT_SPIKE_1 = 32;
+        parameter int NEURON_1 = 64;
+        parameter int NEURON_2 = 128;
+        parameter int NEURON_3 = 64;
+        parameter int NEURON_4 = 16;
+        parameter int CURRENT_DECAY_1 = 4096-4096;
+        parameter int CURRENT_DECAY_2 = 4096-4096;
+        parameter int CURRENT_DECAY_3 = 4096-4096;
+        parameter int CURRENT_DECAY_4 = 4096-4096;
+        parameter int VOLTAGE_DECAY_1 = 4096-41;
+        parameter int VOLTAGE_DECAY_2 = 4096-41;
+        parameter int VOLTAGE_DECAY_3 = 4096-41;
+        parameter int VOLTAGE_DECAY_4 = 4096-41;
+        parameter int THRESHOLD_1 = 39;
+        parameter int THRESHOLD_2 = 29;
+        parameter int THRESHOLD_3 = 32;
+        parameter int THRESHOLD_4 = 32767;
+
+endpackage
